53ee78a64af4c247011555247f0e5d3e09d8283c
tenzija <48459321+tenzija@users.noreply.github.com>
2022-08-25 23:58:36 +0200

    Update index.js

--- components/ui/course/curriculum/index.js
+++ components/ui/course/curriculum/index.js
@@ -1,12 +1,12 @@
 
 
 const lectures = [
-  "How to init App",
-  "How to get a help",
-  "Introduction to Solidity",
-  "Programing in C++",
-  "How to write For Loops",
-  "Safe operator",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 ]
 
 export default function Curriculum({locked}) {
